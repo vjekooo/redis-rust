688c84e8abcc8afc7c43bad4bd05e083b4caebdf
Vjeko <vjeko.pavicin@gmail.com>
2022-04-21 00:26:43 +0200

    respvalues

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use futures::{AsyncReadExt, AsyncWriteExt};
 #[async_std::main]
 async fn main() -> io::Result<()> {
     let mut client = Client::new("localhost:6379").await?;
-    client.set("vjeko".into(), "avion".into()).await.unwrap();
-    println!("{}", client.get("vjeko".into()).await.unwrap());
+    client.set("vjeko".into(), "keks".into()).await.unwrap();
+    println!("Print, {}", client.get("vjeko".into()).await.unwrap());
     Ok(())
 }
 
-fn parse_response(buffer: &[u8]) -> Result<&str, Error> {
+fn parse_response(buffer: &[u8], bytes_read: usize) -> Result<&str, Error> {
+    let first_bytes = 4;
     if buffer.is_empty() {
         return Err(Error {});
     }
@@ -19,7 +20,12 @@ fn parse_response(buffer: &[u8]) -> Result<&str, Error> {
         return Err(Error {});
     }
 
-    Ok(std::str::from_utf8(&buffer[1..buffer.len() - 2]).unwrap())
+    println!(
+        "Parse, {:?}",
+        std::str::from_utf8(&buffer[first_bytes..bytes_read]).unwrap()
+    );
+
+    Ok(std::str::from_utf8(&buffer[first_bytes..bytes_read]).unwrap())
 }
 
 #[derive(Debug)]
@@ -44,44 +50,44 @@ impl Client {
 
 impl Client {
     async fn get(&mut self, key: String) -> Result<String, Error> {
-        let command = ResponseValues::Array(vec![
-            ResponseValues::BulkString(b"GET".to_vec()),
-            ResponseValues::BulkString(key.into_bytes()),
+        let command = RespValues::Array(vec![
+            RespValues::BulkString(b"GET".to_vec()),
+            RespValues::BulkString(key.into_bytes()),
         ]);
         let mut buffer = vec![];
         command.serialize(&mut buffer);
         self.stream.write_all(&buffer).await?;
         let bytes_read = self.stream.read(&mut buffer).await?;
-        let response = parse_response(&buffer[0..bytes_read])?;
+        let response = parse_response(&buffer, bytes_read)?;
         Ok(response.to_owned())
     }
     async fn set(&mut self, key: String, value: String) -> Result<(), Error> {
-        let command = ResponseValues::Array(vec![
-            ResponseValues::BulkString(b"SET".to_vec()),
-            ResponseValues::BulkString(key.into_bytes()),
-            ResponseValues::BulkString(value.into_bytes()),
+        let command = RespValues::Array(vec![
+            RespValues::BulkString(b"SET".to_vec()),
+            RespValues::BulkString(key.into_bytes()),
+            RespValues::BulkString(value.into_bytes()),
         ]);
         let mut buffer = vec![];
         command.serialize(&mut buffer);
         self.stream.write_all(&buffer).await?;
         let bytes_read = self.stream.read(&mut buffer).await?;
-        parse_response(&buffer[0..bytes_read]);
+        parse_response(&buffer, bytes_read);
         Ok(())
     }
 }
 
-enum ResponseValues {
+enum RespValues {
     SimpleString(String),
     Error(Vec<u8>),
     Integer(i64),
     BulkString(Vec<u8>),
-    Array(Vec<ResponseValues>),
+    Array(Vec<RespValues>),
 }
 
-impl ResponseValues {
+impl RespValues {
     fn serialize(self, buffer: &mut Vec<u8>) {
         match self {
-            ResponseValues::Array(values) => {
+            RespValues::Array(values) => {
                 buffer.push(b'*');
                 buffer.append(&mut format!("{}", values.len()).into_bytes());
                 buffer.push('\r' as u8);
@@ -90,7 +96,7 @@ impl ResponseValues {
                     value.serialize(buffer)
                 }
             }
-            ResponseValues::BulkString(mut data) => {
+            RespValues::BulkString(mut data) => {
                 buffer.push(b'$');
                 buffer.append(&mut format!("{}", data.len()).into_bytes());
                 buffer.push('\r' as u8);
